98292448301aa9473bf8f9ed42ce458f54fe2796
alsuqiamneh <81607334+alsuqiamneh@users.noreply.github.com>
2021-05-25 09:01:44 -0700

    Update div.hpp

--- div.hpp
+++ div.hpp
@@ -5,7 +5,7 @@
 #include "iostream"
 #include "sstream"
 #include <iomanip>
-
+#include "visitor.hpp"
 using namespace std;
 
 class Div : public Base {
@@ -79,7 +79,7 @@ virtual Base* get_child(int i) {
         }
     }
 
-virtual void accept(Visitor* visitor, int index){
+void accept(Visitor* visitor, int index){
         if (index == 0) visitor->visit_add_begin(this);
         if (index == 1) visitor->visit_add_middle((this));
         if (index == 2) visitor->visit_add_end(this);
