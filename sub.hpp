6e9b602670111a7d4da79b7902053b0b94fc183f
alsuqiamneh <81607334+alsuqiamneh@users.noreply.github.com>
2021-05-25 08:59:52 -0700

    Update sub.hpp

--- sub.hpp
+++ sub.hpp
@@ -68,7 +68,7 @@ virtual Base* get_child(int i) {
         }
     }
 
-void accept(Visitor* visitor, int index){
+virtual void accept(Visitor* visitor, int index){
         if (index == 0) visitor->visit_add_begin(this);
         if (index == 1) visitor->visit_add_middle((this));
         if (index == 2) visitor->visit_add_end(this);
